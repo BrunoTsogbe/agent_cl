0d20023612e9564b27a5ef0587fb77449c37fecc
Hakim <horairyhakim@gmail.com>
2025-12-08 17:05:28 +0100

    feature: select LLM model

---
+++ config.py
@@ -0,0 +1,8 @@
+LLM_MODELS = [
+	"llama-3.1-8b-instant",
+	"meta-llama/llama-4-scout-17b-16e-instruct"
+	"openai/gpt-oss-20b",
+	"openai/gpt-oss-120b",
+	"llama-3.3-70b-versatile",
+	"moonshotai/kimi-k2-instruct-0905",
+]
